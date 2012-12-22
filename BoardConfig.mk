e79041d5ecab9e457f4f19ecbd8ddb22eba0b5e6
sconosciuto <sconosciuto.git@gmail.com>
2012-12-22 14:36:31 +0100

    latona: use releasetools extensions
    Adapted from Aries

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun%d/file"
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxysl/releasetools/galaxysl_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxysl/releasetools/galaxysl_img_from_target_files
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/galaxysl
 
 # Vibrator
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/galaxysl/vibrator/tspdrv.c
