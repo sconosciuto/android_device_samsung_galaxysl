5943d356f5184cb5379aa0cfb4f8af068aaadc7b
sconosciuto <sconosciuto.git@gmail.com>
2013-07-18 23:09:13 +0200

    latona: use EGL context_priority extension
    Hibernation is not supported.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,15 +74,8 @@ BOARD_HAVE_BLUETOOTH := true
 # Egl
 BOARD_EGL_CFG := device/samsung/galaxysl/egl.cfg
 USE_OPENGL_RENDERER := true
-
-# TARGET_DISABLE_TRIPLE_BUFFERING can be used to disable triple buffering
-# on per target basis. On crespo it is possible to do so in theory
-# to save memory, however, there are currently some limitations in the
-# OpenGL ES driver that in conjunction with disable triple-buffering
-# would hurt performance significantly (see b/6016711)
 TARGET_DISABLE_TRIPLE_BUFFERING := false
-
-BOARD_ALLOW_EGL_HIBERNATION := true
+COMMON_GLOBAL_CFLAGS += -DHAS_CONTEXT_PRIORITY
 
 # OMX Stuff
 HARDWARE_OMX := true
