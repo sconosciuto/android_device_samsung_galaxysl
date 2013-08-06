389cce57e6fce153d7995b258596c67b9f7131cc
sconosciuto <sconosciuto.git@gmail.com>
2013-08-06 17:29:51 +0200

    libcamera: fix preview framesize

--- libcamera/CameraHardware.cpp
+++ libcamera/CameraHardware.cpp
@@ -721,8 +721,7 @@ status_t CameraHardware::startPreviewInternal()
 {
     int ret = 0;
     int fps = mParameters.getPreviewFrameRate();
-    int mRecordingFrameSize;
-    int mPreviewFrameSize;
+    int mFrameSize;
 
     mParameters.getPreviewSize(&mPreviewWidth, &mPreviewHeight);
     ALOGD("startPreview width:%d,height:%d", mPreviewWidth, mPreviewHeight);
@@ -731,14 +730,13 @@ status_t CameraHardware::startPreviewInternal()
         return INVALID_OPERATION;
     }
 
-    mPreviewFrameSize = mPreviewWidth * mPreviewHeight * 1.5;
-    mRecordingFrameSize = mPreviewWidth * mPreviewHeight * 2;
+    mFrameSize = mPreviewWidth * mPreviewHeight * 2;
 
     if (mPreviewHeap) {
         mPreviewHeap->release(mPreviewHeap);
         mPreviewHeap = NULL;
     }
-    mPreviewHeap = mRequestMemory(-1, mPreviewFrameSize, NB_BUFFER, NULL);
+    mPreviewHeap = mRequestMemory(-1, mFrameSize, NB_BUFFER, NULL);
 
     for (int i = 0; i < NB_BUFFER; i++) {
         if (mRecordHeap[i] != NULL) {
@@ -746,7 +744,7 @@ status_t CameraHardware::startPreviewInternal()
             mRecordHeap[i] = 0;
         }
         //mRecordBufferState[i] = 0;
-        mRecordHeap[i] = mRequestMemory(-1, mRecordingFrameSize, 1, NULL);
+        mRecordHeap[i] = mRequestMemory(-1, mFrameSize, 1, NULL);
     }
 
     if (mCameraID == CAMERA_FF) {
@@ -756,13 +754,13 @@ status_t CameraHardware::startPreviewInternal()
             mScaleHeap->release(mScaleHeap);
             mScaleHeap = NULL;
         }
-        mScaleHeap = mRequestMemory(-1, mRecordingFrameSize, 1, NULL);
+        mScaleHeap = mRequestMemory(-1, mFrameSize, 1, NULL);
 
         if (mFrameScaled) {
             mFrameScaled->release(mFrameScaled);
             mFrameScaled = NULL;
         }
-        mFrameScaled = mRequestMemory(-1, mRecordingFrameSize, 1, NULL);
+        mFrameScaled = mRequestMemory(-1, mFrameSize, 1, NULL);
     }
 
     ret = mCamera->Configure(mPreviewWidth, mPreviewHeight, PIXEL_FORMAT, fps, 0);
